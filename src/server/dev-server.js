2a83e739819e388b3e8ae448cc342b4f669a2314
Wei Jian Gan <weijiangan@outlook.com>
2019-02-27 05:42:01 +0800

    rename dev server file and enable set port from env var

--- src/server/dev-server.js
+++ src/server/dev-server.js
@@ -19,4 +19,7 @@ app.use(webpackHotMiddleware(compiler));
 
 app.use("/", express.static(path.join(__dirname, "..", "..", "dist")));
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.set("port", process.env.PORT || 3000);
+app.listen(app.get("port"), () => {
+  console.log(`Server started: http://localhost:${app.get("port")}/`);
+});
