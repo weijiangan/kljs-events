a2fc84e3bc1d70395faf82ee969ba02b9a273622
Wei Jian Gan <weijiangan@outlook.com>
2019-05-20 23:16:00 +0800

    good WIP savepoint. Landing page looks decent now

--- src/client/App.js
+++ src/client/App.js
@@ -1,192 +1,27 @@
 import React from "react";
 import { hot } from "react-hot-loader/root";
-import { Query, Mutation } from "react-apollo";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import gql from "graphql-tag";
-import { format } from "date-fns";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement
-} from "react-vertical-timeline-component";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClock } from "@fortawesome/free-regular-svg-icons";
+import { faClock, faCalendar } from "@fortawesome/free-regular-svg-icons";
 import { faMapMarkedAlt } from "@fortawesome/free-solid-svg-icons";
-import "!style-loader!css-loader!react-vertical-timeline-component/style.min.css";
-import styles from "./app.css";
+import LandingPage from "./LandingPage";
+import "./app.css";
 
-library.add(faClock, faMapMarkedAlt);
+library.add(faClock, faCalendar, faMapMarkedAlt);
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql"
 });
 
-const GET_LATEST_EVENT = gql`
-  {
-    events {
-      name
-      timeStart
-      timeEnd
-      venue {
-        name
-        address
-        googPlaceId
-        coords {
-          x
-          y
-        }
-      }
-      talks {
-        id
-        title
-        speaker {
-          id
-          name
-          email
-        }
-        description
-        length
-        remarks
-      }
-    }
-  }
-`;
-
-const FUCK = gql`
-  mutation Test($eventId: String!, $venue: VenueInput!) {
-    updateEvent(eventId: $eventId, venue: $venue) {
-      id
-      name
-      venue {
-        id
-        name
-        address
-      }
-    }
-  }
-`;
-
 const App = () => (
-  <Router>
-    <Content />
-  </Router>
-);
-
-const Content = () => (
   <ApolloProvider client={client}>
-    <div>
-      <div className={styles.topBar}>
-        <div className={styles.logo}>KLJS</div>
-        <div className={styles.flexCenter}>
-          <button type="button" className={styles.topButton}>
-            Support us!
-          </button>
-        </div>
-      </div>
-      <div
-        className={styles.picture}
-        style={{ height: `calc(100vh - 280px)` }}
-      />
-      <div className={styles.hero}>
-        <div className={styles.row}>
-          <div className={styles.col}>
-            <div className={styles.tabActive}>Next Event</div>
-          </div>
-          <div className={styles.col}>
-            <div className={styles.tab}>Past Events</div>
-          </div>
-          <div className={styles.col}>
-            <div className={styles.tab}>Talks</div>
-          </div>
-        </div>
-        <Query query={GET_LATEST_EVENT}>
-          {({ loading, error, data }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error</p>;
-
-            const event = data.events[0];
-            const dateStart = new Date(event.timeStart * 1000);
-            const dateEnd = new Date(event.timeEnd * 1000);
-            return (
-              <div className={styles.container}>
-                <h1 className={styles.heroTitle}>{event.name}</h1>
-                <div
-                  className={styles.flex}
-                  style={{ marginBottom: "0.75rem" }}
-                >
-                  <div className={styles.iconBullet}>
-                    <FontAwesomeIcon icon={["far", "clock"]} />
-                  </div>
-                  <div>{`${format(dateStart, "H:mm")} - ${format(
-                    dateEnd,
-                    "H:mm"
-                  )}`}</div>
-                </div>
-                <div className={styles.flex}>
-                  <div className={styles.iconBullet}>
-                    <FontAwesomeIcon icon={["fas", "map-marked-alt"]} />
-                  </div>
-                  <div>
-                    <div>{event.venue.name}</div>
-                    <div
-                      dangerouslySetInnerHTML={{
-                        __html: event.venue.address.replace(/\n/g, "<br/>")
-                      }}
-                    />
-                  </div>
-                </div>
-                <VerticalTimeline>
-                  <VerticalTimelineElement
-                    date="7.30 PM"
-                    iconStyle={{
-                      background: "rgb(33, 150, 243)",
-                      color: "#fff"
-                    }}
-                  >
-                    <h3 className="vertical-timeline-element-title">
-                      Networking + Light Food/Snacks
-                    </h3>
-                    <p>Food sponsored by Vase.ai</p>
-                  </VerticalTimelineElement>
-                  {event.talks.map((talk, index) => (
-                    <VerticalTimelineElement
-                      date="8.00 PM"
-                      iconStyle={{
-                        background: "rgb(33, 150, 243)",
-                        color: "#fff"
-                      }}
-                    >
-                      <Link to={`talk/${talk.id}`}>
-                        <h3 className={styles.timelineTitle}>{talk.title}</h3>
-                      </Link>
-                      <Link to={`talk/${talk.speaker.id}`}>
-                        <h4 className={styles.timelineSubtitle}>
-                          {talk.speaker.name}
-                        </h4>
-                      </Link>
-                      <p>{talk.description}</p>
-                    </VerticalTimelineElement>
-                  ))}
-                  <VerticalTimelineElement
-                    date="9.30 PM"
-                    iconStyle={{
-                      background: "rgb(33, 150, 243)",
-                      color: "#fff"
-                    }}
-                  >
-                    <h3 className="vertical-timeline-element-title">
-                      Mingle and go home
-                    </h3>
-                  </VerticalTimelineElement>
-                </VerticalTimeline>
-              </div>
-            );
-          }}
-        </Query>
-      </div>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={LandingPage} />
+      </Switch>
+    </Router>
   </ApolloProvider>
 );
 
---
+++ src/client/LandingPage/index.jsx
@@ -0,0 +1,207 @@
+import React from "react";
+import { Query, Mutation } from "react-apollo";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { format } from "date-fns";
+import gql from "graphql-tag";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement
+} from "react-vertical-timeline-component";
+import "!style-loader!css-loader!react-vertical-timeline-component/style.min.css";
+import styles from "./style.css";
+
+const GET_LATEST_EVENT = gql`
+  {
+    events {
+      name
+      timeStart
+      venue {
+        name
+        address
+        googPlaceId
+        coords {
+          x
+          y
+        }
+      }
+      agenda {
+        order
+        activity {
+          id
+          type
+          title
+          description
+          length
+        }
+      }
+    }
+  }
+`;
+
+const FUCK = gql`
+  mutation Test($eventId: String!, $venue: VenueInput!) {
+    updateEvent(eventId: $eventId, venue: $venue) {
+      id
+      name
+      venue {
+        id
+        name
+        address
+      }
+    }
+  }
+`;
+
+const Content = () => (
+  <Layout>
+    <EventFetcher>
+      {event => (
+        <>
+          <div className={styles.bigEventCard}>
+            <div className={styles.picture} />
+            <div className={styles.hero} />
+            <NavMenu />
+            <EventInfo event={event} />
+          </div>
+          <Timeline event={event} />
+        </>
+      )}
+    </EventFetcher>
+  </Layout>
+);
+
+const EventFetcher = ({ children }) => (
+  <Query query={GET_LATEST_EVENT}>
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error: {error}</p>;
+      const event = data.events[0];
+      let tally = event.timeStart;
+      event.agenda.forEach(({ activity }) => {
+        activity.time = tally;
+        tally = activity.time + activity.length * 60;
+      });
+      event.timeEnd = tally;
+
+      return children(event);
+    }}
+  </Query>
+);
+
+const NavMenu = () => (
+  <div className={styles.row}>
+    <div className={styles.col}>
+      <div className={styles.tabActive}>Next Event</div>
+    </div>
+    <div className={styles.col}>
+      <Link to="events">
+        <div className={styles.tab}>Past Events</div>
+      </Link>
+    </div>
+    <div className={styles.col}>
+      <Link to="/talks">
+        <div className={styles.tab}>Talks</div>
+      </Link>
+    </div>
+  </div>
+);
+
+const EventInfo = ({ event }) => {
+  const dateStart = new Date(event.timeStart * 1000);
+  const dateEnd = new Date(event.timeEnd * 1000);
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heroTitle}>{event.name}</h1>
+      <div className={styles.flex} style={{ marginBottom: "0.75rem" }}>
+        <div className={styles.iconBullet}>
+          <FontAwesomeIcon icon={["far", "calendar"]} />
+        </div>
+        <div>{format(dateStart, "D MMMM YYYY (dddd)")}</div>
+      </div>
+      <div className={styles.flex} style={{ marginBottom: "0.75rem" }}>
+        <div className={styles.iconBullet}>
+          <FontAwesomeIcon icon={["far", "clock"]} />
+        </div>
+        <div>{`${format(dateStart, "h:mm")} - ${format(
+          dateEnd,
+          "h:mm A"
+        )}`}</div>
+      </div>
+      <div className={styles.flex}>
+        <div className={styles.iconBullet}>
+          <FontAwesomeIcon icon={["fas", "map-marked-alt"]} />
+        </div>
+        <div>
+          <div>{event.venue.name}</div>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: event.venue.address.replace(/\n/g, "<br/>")
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Timeline = ({ event }) => (
+  <div
+    style={{
+      boxSizing: "content-box",
+      backgroundColor: "var(--gray)",
+      padding: "2rem 0"
+    }}
+  >
+    <div className={styles.container}>
+      <h2>Agenda</h2>
+    </div>
+    <VerticalTimeline>
+      {event.agenda.map(({ activity }, index) => (
+        <VerticalTimelineElement
+          date={format(new Date(activity.time * 1000), "h:mm A")}
+          iconStyle={{
+            background: "rgb(33, 150, 243)",
+            color: "#fff"
+          }}
+        >
+          {activity.type === "TALK" ? (
+            <Link to={`talk/${activity.id}`}>
+              <h3 className={styles.timelineTitle}>{activity.title}</h3>
+            </Link>
+          ) : (
+            <h3 className={styles.timelineTitle}>{activity.title}</h3>
+          )}
+          {activity.type === "TALK" ? (
+            <Link
+              to={`activity/${activity.speaker ? activity.speaker.id : ""}`}
+            >
+              <h4 className={styles.timelineSubtitle}>
+                {activity.speaker ? activity.speaker.name : "Unkown speaker"}
+              </h4>
+            </Link>
+          ) : (
+            undefined
+          )}
+          {activity.description ? <p>{activity.description}</p> : undefined}
+        </VerticalTimelineElement>
+      ))}
+    </VerticalTimeline>
+  </div>
+);
+
+const Layout = ({ children }) => (
+  <div>
+    <div className={styles.topBar}>
+      <div className={styles.logo}>KLJS</div>
+      <div className={styles.flexCenter}>
+        <button type="button" className={styles.topButton}>
+          Support us!
+        </button>
+      </div>
+    </div>
+    {children}
+  </div>
+);
+
+export default Content;
