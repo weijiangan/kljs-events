979684bc1aedc163327fd13a12ca725efd92a613
Wei Jian Gan <weijiangan@outlook.com>
2019-05-22 16:32:57 +0800

    "eject" apollo-boost

--- src/client/App.js
+++ src/client/App.js
@@ -1,21 +1,17 @@
 import React, { lazy, Suspense } from "react";
 import { hot } from "react-hot-loader/root";
-import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faClock, faCalendar } from "@fortawesome/free-regular-svg-icons";
 import { faMapMarkedAlt } from "@fortawesome/free-solid-svg-icons";
+import { client } from "./ApolloClient";
 import "./app.css";
 
 const LandingPage = lazy(() => import("./LandingPage"));
 const TalkPage = lazy(() => import("./Talk"));
 
 library.add(faClock, faCalendar, faMapMarkedAlt);
-
-const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
-});
 
 const App = () => (
   <ApolloProvider client={client}>
