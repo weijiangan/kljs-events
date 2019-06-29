4908a0d664b67cbdf914a972747f8b81aad64ac2
Wei Jian Gan <weijiangan@outlook.com>
2019-06-30 03:13:49 +0800

    add latestEvent gql query

--- src/server/resolvers/Query.js
+++ src/server/resolvers/Query.js
@@ -18,9 +18,29 @@ function talk(parent, args, context, info) {
   return context.prisma.talk({ id: args.id });
 }
 
+async function latestEvent(parent, args, context, info) {
+  const nowSeconds = new Date().getTime() / 1000;
+  let event = await context.prisma.events({
+    // Show upcoming event 2 hours after the current one so it won't show
+    // future events while there's one ongoing. Should change this when
+    // I find a better approach
+    where: { timeStart_gt: Math.trunc(nowSeconds) + 7200 },
+    orderBy: "timeStart_ASC",
+    first: 1
+  });
+  if (!event.length) {
+    event = await context.prisma.events({
+      orderBy: "timeStart_DESC",
+      first: 1
+    });
+  }
+  return event[0];
+}
+
 module.exports = {
   events,
   users,
   talks,
-  talk
+  talk,
+  latestEvent
 };
