e63a2b4364777802aaa023278ea893d8eb983f10
Wei Jian Gan <weijiangan@outlook.com>
2019-07-24 17:39:07 +0800

    add optimized scroll event

--- src/client/index.js
+++ src/client/index.js
@@ -2,4 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+var throttle = function(type, name, obj) {
+  obj = obj || window;
+  var running = false;
+  var func = function() {
+    if (running) {
+      return;
+    }
+    running = true;
+    requestAnimationFrame(function() {
+      obj.dispatchEvent(new CustomEvent(name));
+      running = false;
+    });
+  };
+  obj.addEventListener(type, func);
+};
+
+/* init - you can init any event */
+throttle("scroll", "optimizedScroll");
+
 ReactDOM.render(<App />, document.getElementById("root"));
