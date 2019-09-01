a6359fbc8274f4f798773e7f43ae2f70540d9a99
Wei Jian Gan <weijiangan@outlook.com>
2019-09-01 18:07:13 +0800

    stop logging GraphQL errors to console

--- src/client/ApolloClient.js
+++ src/client/ApolloClient.js
@@ -14,15 +14,15 @@ const fragmentMatcher = new IntrospectionFragmentMatcher({
 
 const client = new ApolloClient({
   link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.map(({ message, locations, path }) =>
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-          )
-        );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
-    }),
+    // onError(({ graphQLErrors, networkError }) => {
+    //   if (graphQLErrors)
+    //     graphQLErrors.map(({ message, locations, path }) =>
+    //       console.log(
+    //         `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+    //       )
+    //     );
+    //   if (networkError) console.log(`[Network error]: ${networkError}`);
+    // }),
     new HttpLink({
       uri: "http://localhost:4000/graphql",
       credentials: "same-origin"
